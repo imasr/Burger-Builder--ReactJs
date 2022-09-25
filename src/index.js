f7239b769a193c32e2736771a25751d6d2997a43
imasr <ashishs723@gmail.com>
2022-09-25 19:34:36 +0530

    set height

--- src/index.js
+++ src/index.js
@@ -23,7 +23,9 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
