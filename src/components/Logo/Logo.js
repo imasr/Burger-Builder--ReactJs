a313dea8513838801d492f4fb264a84e168c4864
imasr <ashishs723@gmail.com>
2022-09-25 20:18:04 +0530

    some change

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,13 @@
-import React from "react"
+import React from "react";
 
-import classes from "./Logo.module.scss"
+import "./Logo.scss";
 
-import BurgerLogo from "./../../assets/images/burger-logo.png.png"
+import BurgerLogo from "./../../assets/images/burger-logo.png";
 
 const logo = (props) => (
-    <div className={classes.Logo} style={{ height: props.height }}>
-        <img src={BurgerLogo} alt="burger" />
-    </div>
-)
+  <div className="logo-icon" style={{ height: props.height }}>
+    <img src={BurgerLogo} alt="burger" />
+  </div>
+);
 
-export default logo
+export default logo;
