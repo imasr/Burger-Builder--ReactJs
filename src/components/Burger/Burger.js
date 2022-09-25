5decefdfeb1083433226f7f1b0571d482ed41c6d
imasr <ashishs723@gmail.com>
2022-09-25 19:55:20 +0530

    some change

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from "react";
 
-import Classes from './Burger.module.scss';
+import Classes from "./Burger.module.scss";
 
-import BurgerIngredient from './Burger-Ingredient/Burger-Ingredient'
+import BurgerIngredient from "./Burger-Ingredient/Burger-Ingredient";
 
 const burger = (props) => {
-    let transformedIngredient = Object.keys(props.ingredients)
-        .map(key => {
-            return [...Array(props.ingredients[key])].map((_, i) => {
-                return <BurgerIngredient key={key + i} type={key} />
-            })
-        })
-        .reduce((prev, current) => prev.concat(current), [])
-    console.log(transformedIngredient);
-    if (transformedIngredient.length === 0) {
-        transformedIngredient = 'Please start adding ingredients'
-    }
+  const ingredients = props.ingredients;
+  let transformedIngredient = Object.keys(ingredients)
+    .map((key) => {
+      return [...Array(ingredients[key])].map((_, i) => {
+        return <BurgerIngredient key={key + i} type={key} />;
+      });
+    })
+    .reduce((prev, current) => prev.concat(current), []);
+  console.log(transformedIngredient);
+  if (transformedIngredient.length === 0) {
+    transformedIngredient = "Please start adding ingredients";
+  }
 
-    return (
-        <div className={Classes.Burger}>
-            <BurgerIngredient type="bread-top" />
-            {transformedIngredient}
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    )
-}
+  return (
+    <div className={Classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredient}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
 
-export default burger
+export default burger;
