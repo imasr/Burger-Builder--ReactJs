042d56217129f7f802ca9037936ac96cca70cbdf
imasr <ashishs723@gmail.com>
2022-09-25 19:07:58 +0530

    updated to 18.2.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import './App.scss';
+import "./App.scss";
 
 import Layout from "./hoc/Layout/Layout";
 import { Checkout } from "./containers/Checkout/Checkout";
 import { BurgerBuilder } from "./containers/Burger-Builder/Burger-Builder";
 
-
 function App() {
   return (
-    <div >
+    <div>
       <Layout>
-        <Switch>
-          <Route path="/" exact component={BurgerBuilder} />
-          <Route path="/checkout" component={Checkout} />
-        </Switch>
+        <Routes>
+          <Route path="/" exact element={<BurgerBuilder />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
       </Layout>
-    </div >
+    </div>
   );
 }
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
-import classes from "./Layout.module.scss"
+import React, { Component } from "react";
+import classes from "./Layout.module.scss";
 
 import Wrapper from "../Wrapper/Wrapper";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-import { BurgerBuilder } from "../../containers/Burger-Builder/Burger-Builder";
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
-
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
-    state = {
-        showSideDrawer: false
-    }
+  state = {
+    showSideDrawer: false,
+  };
 
-    sideDrawerClose = () => {
-        this.setState({ showSideDrawer: false })
-    }
+  sideDrawerClose = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-    sideDrawerToggler = () => {
-        this.setState((prevState) => {
-            return { showSideDrawer: !prevState.showSideDrawer }
-        })
-    }
+  sideDrawerToggler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
 
-    render() {
-        return (
-            <Wrapper>
-                <Toolbar toggleSideDrawer={this.sideDrawerToggler} />
-                <SideDrawer show={this.state.showSideDrawer} close={this.sideDrawerClose} />
-                <main className={classes.Content}>
-                    {/* <BurgerBuilder /> */}
-                    {this.props.children}
-                </main>
-            </Wrapper>
-        )
-    }
+  render() {
+    return (
+      <Wrapper>
+        <Toolbar toggleSideDrawer={this.sideDrawerToggler} />
+        <SideDrawer
+          show={this.state.showSideDrawer}
+          close={this.sideDrawerClose}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Wrapper>
+    );
+  }
 }
-export default Layout
+export default Layout;
